fb5dc9a816e5b7731de062b08ab1f798ebaf0043
Yakimory <yastrebdev@gmail.com>
2024-10-03 13:15:30 +0700

    game is ready

--- game_process.py
+++ game_process.py
@@ -21,17 +21,31 @@ def game_process(*, players, board, fields):
         print(f'Ходит игрок {player.username}: {player.move_type}')
         print(f'Текущее поле {player.current_field}')
 
-        open_fields = sum(sum(row) for row in fields)
-
         cell = move_input()
         if not cell: continue
 
         is_move = player.move(fields, board, cell)
         result = check_field(board, player.current_field, fields)
-        if result:
-            score[result] += 1
 
         if is_move:
+            if result:
+                score[result] += 1
+            print(score)
+
+            if score['X'] == 5:
+                winner = 'X'
+            elif score['O'] == 5:
+                winner = 'O'
+
+            open_fields = sum(sum(row) for row in fields)
+            if open_fields == 0:
+                if score['X'] > score['O']:
+                    winner = 'X'
+                elif score['O'] > score['X']:
+                    winner = 'O'
+                else:
+                    break
+
             empty_cell -= 1
             random_start_player += 1
             print_board(board)
@@ -45,19 +59,6 @@ def game_process(*, players, board, fields):
                 if not field:
                     continue
                 is_field = players[random_start_player % 2].field_selection(fields, field)
-
-        if score['X'] == 5:
-            winner = 'X'
-        elif score['O'] == 5:
-            winner = 'O'
-
-        if open_fields == 0:
-            if score['X'] > score['O']:
-                winner = 'X'
-            elif score['O'] > score['X']:
-                winner = 'O'
-            else:
-                break
 
         if empty_cell == 0:
             break
